5b352fa782f00f28ff5dd06e5e5b397199c57ad8
Aayush Thakur <aayush@ini8labs.tech>
2023-04-26 02:10:39 -0700

    refactor: updated AddNewEvent func to perform checks

--- src/apis/admin.go
+++ src/apis/admin.go
@@ -3,6 +3,7 @@ package apis
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ini8labs/lsdb"
@@ -24,14 +25,14 @@ func NewServer(server Server) error {
 	//r.GET("/api/v1/user/info", GetAllUserData)
 	r.GET("/api/v1/userinfo/Phone", server.GetUserInfoByPhone)
 	r.GET("/api/v1/userinfo/Gov_Id", server.GetUserInfoByGovID)
-	r.GET("/api/v1/userinfo/UID", server.GetUserInfoByID) //not working
-	r.GET("/api/v1/userinfo/EventID", server.GetParticipantsInfoByEventID)
+	r.GET("/api/v1/userinfo/UID", server.GetUserInfoByID)
+	//r.GET("/api/v1/userinfo/EventID", server.GetParticipantsInfoByEventID)
 
 	r.GET("/api/v1/eventinfo", server.GetAllEvents)
 	r.GET("api/v1/eventinfo/Eventtype", server.GetEventsByType)
 	// r.GET("/api/v1/eventinfo/Date", server.GetEventsByDate)                  need to convert string to primitive.Date
 	// r.GET("/api/v1/eventinfo/Daterange", server.GetEventsByDateRange)        need to convert string to primitive.Date
-	r.GET("/api/v1/eventinfo/Winners", server.GetEventWinners) // will not work
+	//r.GET("/api/v1/eventinfo/Winners", server.GetEventWinners) // will not work
 
 	r.POST("/api/v1/event/Add", server.AddNewEvent)
 	r.DELETE("/api/v1/event/Delete", server.DeleteEvent)
@@ -40,31 +41,31 @@ func NewServer(server Server) error {
 }
 
 type UserInfoByEventId struct {
-	EventUID   primitive.ObjectID `bson:"_id,omitempty"`
-	EventDate  primitive.DateTime `bson:"event_date,omitempty"`
-	UserName   string             `bson:"name,omitempty"`
-	EventName  string             `bson:"name,omitempty"`
-	EventType  string             `bson:"event_type,omitempty"`
-	BetUID     primitive.ObjectID `bson:"_id,omitempty"`
-	UserID     primitive.ObjectID `bson:"user_id,omitempty"`
-	BetNumbers []int              `bson:"bet_numbers,omitempty"`
-	Amount     int                `bson:"amount,omitempty"`
+	EventUID   string    `json:"_id,omitempty"`
+	EventDate  time.Time `json:"event_date,omitempty"`
+	UserName   string    `json:"name,omitempty"`
+	EventName  string    `json:"eventname,omitempty"`
+	EventType  string    `json:"event_type,omitempty"`
+	BetUID     string    `json:"betid,omitempty"`
+	UserID     string    `json:"user_id,omitempty"`
+	BetNumbers []int     `json:"bet_numbers,omitempty"`
+	Amount     int       `json:"amount,omitempty"`
 }
 
 type EventsInfo struct {
-	EventUID      primitive.ObjectID `bson:"_id,omitempty"`
-	EventDate     primitive.DateTime `bson:"event_date,omitempty"`
-	EventName     string             `bson:"name,omitempty"`
-	EventType     string             `bson:"event_type,omitempty"`
-	WinningNumber int                `bson:"winning_number,omitempty"`
+	EventUID      string    `json:"_id,omitempty"`
+	EventDate     time.Time `json:"event_date,omitempty"`
+	EventName     string    `json:"name,omitempty"`
+	EventType     string    `json:"event_type,omitempty"`
+	WinningNumber int       `json:"winning_number,omitempty"`
 }
 
 type UserInfo struct {
-	UID   primitive.ObjectID `bson:"_id,omitempty"`
-	Name  string             `bson:"name,omitempty"`
-	Phone int64              `bson:"phone,omitempty"`
-	GovID string             `bson:"gov_id,omitempty"`
-	EMail string             `bson:"e_mail,omitempty"`
+	UID   string `json:"_id,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Phone int64  `json:"phone,omitempty"`
+	GovID string `json:"gov_id,omitempty"`
+	EMail string `json:"e_mail,omitempty"`
 }
 
 func (s Server) GetUserInfoByPhone(c *gin.Context) {
@@ -75,14 +76,18 @@ func (s Server) GetUserInfoByPhone(c *gin.Context) {
 	resp, err := s.Client.GetUserInfoByPhone(int64(userphonenumber))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err)
+		s.Logger.Error(err.Error())
 		return
 	}
 
 	var userinfo UserInfo
 	if resp.Phone == int64(userphonenumber) {
+
+		mongouserId := resp.UID
+		stringUserID := mongouserId.Hex()
+
 		userinfo.Name = resp.Name
-		userinfo.UID = resp.UID
+		userinfo.UID = stringUserID
 		userinfo.Phone = resp.Phone
 		userinfo.GovID = resp.GovID
 		userinfo.EMail = resp.EMail
@@ -97,13 +102,16 @@ func (s Server) GetUserInfoByGovID(c *gin.Context) {
 	resp, err := s.Client.GetUserInfoByGovID(govid)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err)
+		s.Logger.Error(err.Error())
 		return
 	}
 	var userinfo UserInfo
 	if resp.GovID == govid {
+		mongouserId := resp.UID
+		stringUserID := mongouserId.Hex()
+
 		userinfo.Name = resp.Name
-		userinfo.UID = resp.UID
+		userinfo.UID = stringUserID
 		userinfo.Phone = resp.Phone
 		userinfo.GovID = resp.GovID
 		userinfo.EMail = resp.EMail
@@ -116,20 +124,24 @@ func (s Server) GetUserInfoByID(c *gin.Context) {
 	uid := c.Query("uid")
 	objID, err := primitive.ObjectIDFromHex(uid)
 	if err != nil {
-		logrus.Infoln("Provided hex string is not a valid ObjectID")
+		s.Logger.Error(err.Error())
 		return
 	}
 
 	resp, err1 := s.Client.GetUserInfoByID(objID)
 	if err1 != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err1)
+		s.Logger.Error(err.Error())
 		return
 	}
 	var userinfo UserInfo
 	if resp.UID == objID {
+
+		mongouserId := resp.UID
+		stringUserID := mongouserId.Hex()
+
 		userinfo.Name = resp.Name
-		userinfo.UID = resp.UID
+		userinfo.UID = stringUserID
 		userinfo.Phone = resp.Phone
 		userinfo.GovID = resp.GovID
 		userinfo.EMail = resp.EMail
@@ -143,21 +155,30 @@ func (s Server) GetAllEvents(c *gin.Context) {
 	resp, err := s.Client.GetAllEvents()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err)
+		s.Logger.Error(err.Error())
 		return
 	}
 
-	var eventinfo EventsInfo
+	var arr []EventsInfo
 
 	for i := 0; i < len(resp); i++ {
-		eventinfo.EventUID = resp[i].EventUID
-		eventinfo.EventDate = resp[i].EventDate
+		var eventinfo EventsInfo
+
+		mongouserId := resp[i].EventUID
+		stringEventID := mongouserId.Hex()
+		eventinfo.EventUID = stringEventID
+
+		mongoEventDate := resp[i].EventDate
+		stringEvenDate := mongoEventDate.Time()
+		eventinfo.EventDate = stringEvenDate
+
 		eventinfo.EventName = resp[i].Name
 		eventinfo.EventType = resp[i].EventType
 		eventinfo.WinningNumber = resp[i].WinningNumber
 
-		c.JSON(http.StatusOK, eventinfo)
+		arr = append(arr, eventinfo)
 	}
+	c.JSON(http.StatusOK, arr)
 }
 
 func (s Server) GetEventsByType(c *gin.Context) {
@@ -167,144 +188,244 @@ func (s Server) GetEventsByType(c *gin.Context) {
 	resp, err := s.Client.GetEventsByType(eventtype)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err)
+		s.Logger.Error(err.Error())
 		return
 	}
 
-	var eventinfo EventsInfo
+	var arr []EventsInfo
+
 	for i := 0; i < len(resp); i++ {
-		eventinfo.EventUID = resp[i].EventUID
-		eventinfo.EventDate = resp[i].EventDate
+		var eventinfo EventsInfo
+
+		mongoEventId := resp[i].EventUID
+		stringEventID := mongoEventId.Hex()
+		eventinfo.EventUID = stringEventID
+
+		mongoEventDate := resp[i].EventDate
+		stringEvenDate := mongoEventDate.Time()
+		eventinfo.EventDate = stringEvenDate
+
 		eventinfo.EventName = resp[i].Name
 		eventinfo.EventType = resp[i].EventType
 		eventinfo.WinningNumber = resp[i].WinningNumber
 
-		c.JSON(http.StatusOK, eventinfo)
+		arr = append(arr, eventinfo)
 	}
+	c.JSON(http.StatusOK, arr)
 
 }
 
-// func (s Server) GetEventsByDate(c *gin.Context) {
-// 	date := c.Query("date")
+// // func (s Server) GetEventsByDate(c *gin.Context) {
+// // 	date := c.Query("date")
+// // 	eventdate := primitive.NewDateTimeFromTime(time.Date(2023, time.April, 24, 0, 0, 0, 0, time.Local))
 
-// 	resp, err := s.Client.GetEventsByDate(date)
+// // 	resp, err := s.Client.GetEventsByDate(eventdate)
+// // 	if err != nil {
+// // 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
+// // 		return
+// // 	}
+
+// // 	var eventinfo EventsInfo
+
+// // 	for i := 0; i < len(resp); i++ {
+// // 		eventinfo.EventUID = resp[i].EventUID
+// // 		eventinfo.EventDate = resp[i].EventDate
+// // 		eventinfo.EventName = resp[i].Name
+// // 		eventinfo.EventType = resp[i].EventType
+// // 		eventinfo.WinningNumber = resp[i].WinningNumber
+
+// // 		c.JSON(http.StatusOK, eventinfo)
+// // 	}
+// // }
+
+// // func (s Server) GetEventsByDateRange(c *gin.Context) {
+
+// // 	var eventdaterange EventDateRange
+
+// // 	if err := c.BindJSON(&eventdaterange); err != nil {
+// // 		c.JSON(http.StatusBadRequest, "Bad Format")
+// // 		return
+// // 	}
+
+// // 	resp, err := s.Client.GetEventByDateRange(eventdaterange.StartDate, eventdaterange.EndDate)
+// // 	if err != nil {
+// // 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
+// // 		return
+// // 	}
+// // 	c.JSON(http.StatusOK, resp)
+// // }
+
+// func (s Server) GetParticipantsInfoByEventID(c *gin.Context) {
+
+// 	eventid := c.Query("eventid")
+// 	id, err := primitive.ObjectIDFromHex(eventid)
+// 	if err != nil {
+// 		logrus.Infoln(err)
+// 		return
+// 	}
+
+// 	resp, err1 := s.Client.GetParticipantsInfoByEventID(id)
+// 	if err1 != nil {
+// 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
+// 		logrus.Infoln(err1)
+// 		return
+// 	}
+// 	var arr []UserInfoByEventId
+
+// 	for i := 0; i < len(resp); i++ {
+
+// 		var userinfobyevent UserInfoByEventId
+
+// 		if resp[i].EventUID == id {
+// 			mongouserId := resp[i].UserID
+// 			stringUserID := mongouserId.String()
+// 			userinfobyevent.UserID = stringUserID
+
+// 			mongoeventId := resp[i].EventUID
+// 			stringEventID := mongoeventId.String()
+// 			userinfobyevent.EventUID = stringEventID
+
+// 			mongoBetId := resp[i].BetUID
+// 			stringBetID := mongoBetId.String()
+// 			userinfobyevent.BetUID = stringBetID
+
+// 			userinfobyevent.Amount = resp[i].Amount
+// 			userinfobyevent.BetNumbers = resp[i].BetNumbers
+
+// 			UserID, err2 := primitive.ObjectIDFromHex(userinfobyevent.UserID)
+// 			if err != nil {
+// 				panic(err2)
+// 			}
+
+// 			resp2, err3 := s.Client.GetUserInfoByID(UserID)
+// 			if err3 != nil {
+// 				c.JSON(http.StatusInternalServerError, "something is wrong with the server")
+// 				logrus.Infoln(err2)
+// 				return
+// 			}
+
+// 			if userinfobyevent.UserID == resp2.UID.String() {
+// 				userinfobyevent.UserName = resp2.Name
+// 			}
+
+// 			// resp3, err3 := s.Client.GetUserInfoByID(eventinfo.UserID)
+// 			// if err3 != nil {
+// 			// 	c.JSON(http.StatusInternalServerError, "something is wrong with the server")
+// 			// 	logrus.Infoln(err3)
+// 			// 	return
+// 			// }
+// 			arr = append(arr, userinfobyevent)
+// 		}
+// 	}
+// 	c.JSON(http.StatusOK, arr)
+
+// }
+
+// func (s Server) GetEventWinners(c *gin.Context) {
+// 	eventid := c.Query("eventid")
+// 	id, err := primitive.ObjectIDFromHex(eventid)
+// 	if err != nil {
+// 		logrus.Infoln(err)
+// 	}
+
+// 	resp, err := s.Client.GetEventWinners(id)
 // 	if err != nil {
 // 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
+// 		logrus.Infoln(err)
 // 		return
 // 	}
 // 	c.JSON(http.StatusOK, resp)
 // }
-
-// func (s Server) GetEventsByDateRange(c *gin.Context) {
-
-// 	var eventdaterange EventDateRange
-
-// 	if err := c.BindJSON(&eventdaterange); err != nil {
-// 		c.JSON(http.StatusBadRequest, "Bad Format")
-// 		return
-// 	}
-
-// 	resp, err := s.Client.GetEventByDateRange(eventdaterange.StartDate, eventdaterange.EndDate)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, resp)
-// }
-
-func (s Server) GetParticipantsInfoByEventID(c *gin.Context) {
-
-	eventid := c.Query("eventid")
-	id, err := primitive.ObjectIDFromHex(eventid)
-	if err != nil {
-		logrus.Infoln(err)
-		return
-	}
-
-	resp, err1 := s.Client.GetParticipantsInfoByEventID(id)
-	if err1 != nil {
-		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err1)
-		return
-	}
-	for i := 0; i < len(resp); i++ {
-
-		var userinfobyevent UserInfoByEventId
-		if resp[i].EventUID == id {
-			userinfobyevent.EventUID = resp[i].EventUID
-			userinfobyevent.BetUID = resp[i].BetUID
-			userinfobyevent.UserID = resp[i].UserID
-			userinfobyevent.Amount = resp[i].Amount
-			userinfobyevent.BetNumbers = resp[i].BetNumbers
-
-			resp2, err2 := s.Client.GetUserInfoByID(userinfobyevent.UserID)
-			if err2 != nil {
-				c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-				logrus.Infoln(err2)
-				return
-			}
-			if userinfobyevent.UserID == resp2.UID {
-				userinfobyevent.UserName = resp2.Name
-			}
-
-			// resp3, err3 := s.Client.GetUserInfoByID(eventinfo.UserID)
-			// if err3 != nil {
-			// 	c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-			// 	logrus.Infoln(err3)
-			// 	return
-			// }
-
-			c.JSON(http.StatusOK, userinfobyevent)
-		}
-	}
-
-}
-
-func (s Server) GetEventWinners(c *gin.Context) {
-	eventid := c.Query("eventid")
-	id, err := primitive.ObjectIDFromHex(eventid)
-	if err != nil {
-		logrus.Infoln(err)
-	}
-
-	resp, err := s.Client.GetEventWinners(id)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err)
-		return
-	}
-	c.JSON(http.StatusOK, resp)
-}
 
 func (s Server) AddNewEvent(c *gin.Context) {
 
-	var addevent lsdb.LotteryEventInfo
+	// events := []string{"Monday Special", "Lucky Tuesday", "Midweek", "Fortune Thursday", "Friday Bonanza", "National Weekly"}
+	//eventtypes := []string{"MS", "LT", "MW", "FT", "FB"}
 
-	if err := c.BindJSON(&addevent); err != nil {
-		c.JSON(http.StatusBadRequest, "Bad Format")
-		logrus.Infoln(err)
+	events := map[string]string{
+		"Monday Special":   "MS",
+		"Lucky Tuesday":    "LT",
+		"Midweek":          "MW",
+		"Fortune Thursday": "FT",
+		"Friday Bonanza":   "FB",
+		"National Weekly":  "NW",
+	}
+
+	event_date, exists1 := c.GetQuery("date")
+	name, exists2 := c.GetQuery("name")
+	eventtype, exists3 := c.GetQuery("eventtype")
+	winingnumber, exists4 := c.GetQuery("winningnumber")
+
+	intWinNumber, _ := strconv.Atoi(winingnumber)
+
+	date, error := time.Parse("2006-01-02", event_date)
+	if error != nil {
+		s.Logger.Error("Can't parse date format")
+		c.JSON(http.StatusBadRequest, "Date not in correct format")
 		return
 	}
 
-	if err := s.Client.AddNewEvent(addevent); err != nil {
+	if intWinNumber < 1 || intWinNumber > 90 {
+		c.JSON(http.StatusBadRequest, "Win numbers should be greater than 1 and less than 90")
+		return
+	}
+
+	if _, ok := events[name]; !ok {
+		c.JSON(http.StatusBadRequest, "Invalid Event")
+		return
+	}
+
+	var result bool = false
+	for _, value := range events {
+		if value == eventtype {
+			result = true
+		}
+	}
+
+	if !result {
+		c.JSON(http.StatusBadRequest, "Event type does not exist")
+		return
+	}
+
+	if events[name] != eventtype {
+		c.JSON(http.StatusBadRequest, "Event does not match event type")
+		return
+	}
+
+	eventinfo := lsdb.LotteryEventInfo{
+		Name:          name,
+		EventDate:     primitive.NewDateTimeFromTime(date),
+		WinningNumber: intWinNumber,
+		EventType:     eventtype,
+	}
+	if !exists1 || !exists2 || !exists3 || !exists4 {
+		c.JSON(http.StatusBadRequest, "Bad Format")
+		return
+	}
+
+	if err := s.Client.AddNewEvent(eventinfo); err != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
-		logrus.Infoln(err)
+		s.Logger.Error(err.Error())
 		return
 	}
 	c.JSON(http.StatusOK, "Event added successfully")
 }
 
 func (s Server) DeleteEvent(c *gin.Context) {
+	eventid, exists := c.GetQuery("EventUID")
 
-	var deleteevent lsdb.LotteryEventInfo
-
-	if err := c.BindJSON(&deleteevent); err != nil {
+	if !exists {
+		s.Logger.Error("Field Missing")
 		c.JSON(http.StatusBadRequest, "Bad Format")
-		logrus.Infoln(err)
+	}
+
+	objID, err := primitive.ObjectIDFromHex(eventid)
+	if err != nil {
+		logrus.Infoln("Provided hex string is not a valid ObjectID")
 		return
 	}
 
-	if err := s.Client.DeleteEvent(deleteevent.EventUID); err != nil {
+	if err := s.Client.DeleteEvent(objID); err != nil {
 		c.JSON(http.StatusInternalServerError, "something is wrong with the server")
 		logrus.Infoln(err)
 		return
